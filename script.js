08275cc4715c7636cd461f5ee19d16885d7c9d61
chilimango <maldonadot55@yahoo.com>
2020-07-21 10:22:12 -0700

    combined button function with the for loop

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
-var numChar = prompt("How many characters?")
-///var askLow = confirm("Would you like lower case letters?")
-//var askUpp = confirm("Would you upper case letters?")
 var lowCase = ("abcdefghijklmnopqrstuvwyx".split(""))
 var uppCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""))
 var specChar = ("!@#$%&".split(""))
 var numbers = ("1234567890".split(""))
 
-var password = []
+var button = document.getElementById("button")
+var display = document.getElementById("display")
 
-var display = document.getElementById("#display")
+var password
+var characters
 
-//the max and min limits of the intial prompt
-if (numChar < 8 || numChar > 128) { ;
-    alert("another number") ; 
-} 
-
-for (var i = 0; i < numChar; i++) {
- var passwordAll = lowCase[Math.floor(Math.random() * lowCase.length)] ;
+button.addEventListener("click", function(e) {
+    var numChar = prompt("How many characters?")
+        // if (numChar < 8 || numChar > 128) ;
+        //     alert("another number") ; 
+    var askLow = confirm("Would you like lower case letters?")
+    var askUpp = confirm("Would you upper case letters?")
+    var askSpecChar = confirm("Would you like specials characters?")
+    var askNumbers = confirm("Would you like numbers?") 
         
- password.push(passwordAll) ;
+        if (askLow == true) {
+            characters +=  lowCase ;
+        }
+        else if (askUpp == true) {
+            characters += uppCase ;
+        }
+        else if (askSpecChar == true) {
+            characters += specChar ;
+        }
+        else if (askNumbers == true) {
+            characters += numbers ;
+        }
+    
+    
+ for (var i = 0; i < numChar; i++) {
+    var password = characters[Math.floor(Math.random() * characters.length)] ;
+    console.log(password)
 
- console.log(password.join(""));
+//     password.push(characters)
+//  display.password.join("") ;
 
  }
-     
-    // }
-    //  for (var j = 0; j < numChar; j++) {
-    //  var randomUpp = uppCase[Math.floor(Math.random() * uppCase.length)] 
-        
-    //   password.push(randomUpp)
-    //  }
-    //  for (var i = 0; i < numChar; i++) {
-    //  var randomSpec = specChar[Math.floor(Math.random() * specChar.length)] 
-        
-    //  password.push(randomSpec)
-    // }
-    // for (var i = 0; i < numChar; i++) {
-    // var randomNum = numbers[Math.floor(Math.random() * numbers.length)] 
-        
-    // password.push(randomNum)
+})
+
     
 
     
